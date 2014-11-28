d54770c63bb49a768acb58e7e6a26a5126075358
David Larrabee <david.larrabee@meyouhealth.com>
2014-11-28 08:41:38 -0500

    Require dependencies in redpotion.rb

--- lib/redpotion.rb
+++ lib/redpotion.rb
@@ -4,6 +4,9 @@ unless defined?(Motion::Project::Config)
   raise "This file must be required within a RubyMotion project Rakefile."
 end
 
+require 'ruby_motion_query'
+require 'ProMotion'
+
 lib_dir_path = File.dirname(File.expand_path(__FILE__))
 Motion::Project::App.setup do |app|
   app.files.unshift(Dir.glob(File.join(lib_dir_path, "project/**/*.rb")))
