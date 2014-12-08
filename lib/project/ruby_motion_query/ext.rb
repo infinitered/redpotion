0e6e3d03adb7ebd9a3c1fdaacfb76e3fbe541c48
Todd Werth <todd@infinitered.com>
2014-12-08 09:52:16 -0800

    I think I fixed the weirdness of Gant's and not being able to use the Edge version

--- lib/project/ruby_motion_query/ext.rb
+++ lib/project/ruby_motion_query/ext.rb
@@ -3,7 +3,9 @@ class Object
     rmq.app
   end
 
-  alias :find :rmq
+  def find(*args) # Do not alias this, strange bugs happen where classes don't have methods
+    rmq(args)
+  end
 end
 
 class UIView
