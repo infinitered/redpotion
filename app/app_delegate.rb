cba7373b55450e52d67a9ecabfa02ba02c4c4acb
David Larrabee <david.larrabee@meyouhealth.com>
2014-12-22 15:24:07 -0500

    Update data table sample to use a CDQ based model

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,6 +4,9 @@ class AppDelegate < PM::Delegate
 
   def on_load(app, options)
     cdq.setup
+
+    load_contributers
+
     open HomeScreen.new(nav_bar: true)
   end
 
@@ -12,5 +15,17 @@ class AppDelegate < PM::Delegate
     # Manually set RMQ's orientation before the device is actually oriented
     # So that we can do stuff like style views before the rotation begins
     rmq.device.orientation = new_orientation
+  end
+
+  def load_contributers
+    contributers = %w{twerth squidpunch GantMan shreeve chunlea markrickert}
+
+    if Contributer.count != contributers.count
+      Contributer.destroy_all
+      contributers.each do |c|
+        Contributer.new(name: c)
+      end
+      cdq.save
+    end
   end
 end
--- app/models/contributer.rb
+++ app/models/contributer.rb
@@ -1,17 +1,2 @@
-class Contributer
-  # TODO: when cdq is included some events are not firing correctly.
-  # this works like a CDQ model, but not using CDQ right now without CDQ we can
-  # use create the feature we are trying to build here, but we need to sort out
-  # the CDQ thing
-  attr_accessor :name
-
-  def initialize(properties)
-    self.name = properties[:name]
-  end
-
-  def self.all
-    @all ||= %w{twerth squidpunch GantMan shreeve chunlea markrickert}.collect do |name|
-      Contributer.new(name: name)
-    end
-  end
+class Contributer < CDQManagedObject
 end
