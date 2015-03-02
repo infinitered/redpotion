b51046d5ea6290da1a4895e2464d970b46a428be
Gant <GantMan@gmail.com>
2015-03-02 15:22:47 -0600

    fix by defining new method

--- lib/project/ruby_motion_query/stylers/ui_image_view.rb
+++ lib/project/ruby_motion_query/stylers/ui_image_view.rb
@@ -3,14 +3,18 @@ module RubyMotionQuery
 
     class UIImageViewStyler < UIViewStyler
 
-      alias :"placeholder_image=" :"image="
       def remote_image=(value)
         if @view.respond_to?("setImageWithURL:placeholder:")
           value = NSURL.URLWithString(value) unless value.is_a?(NSURL)
           @view.setImageWithURL(value, placeholder:@view.image)
         else
-          puts "\n[RMQ ERROR]  tried to set remote_image without JMImageCache cocoapod. Please add this to your Rakefile: \n\napp.pods do\n  pod \"JMImageCache\"\nend\n"
+          puts "\n[RedPotion ERROR]  tried to set remote_image without JMImageCache cocoapod. Please add this to your Rakefile: \n\napp.pods do\n  pod \"JMImageCache\"\nend\n"
         end
+      end
+
+      # This is the same functionality as image=
+      def placeholder_image=(value)
+        @view.image = value
       end
     end
   end
