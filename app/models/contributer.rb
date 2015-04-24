993c7812a6e6aaa2ae4214101abb63bfa3551cc8
Mark Rickert <mjar81@gmail.com>
2015-04-23 23:13:49 -0400

    Adds a :created_at timestamp to Contributor model.

--- app/models/contributer.rb
+++ app/models/contributer.rb
@@ -1,5 +1,4 @@
 class Contributer < CDQManagedObject
-  scope :all, sort_by(:name)
   scope :starts_with_s, where(:name).begins_with('s').sort_by(:name)
 
   def cell
--- schemas/0001_initial.rb
+++ schemas/0001_initial.rb
@@ -1,5 +1,7 @@
 schema "0001 initial" do
   entity "Contributer" do
     string :name, optional: false
+
+    datetime :created_at
   end
 end
