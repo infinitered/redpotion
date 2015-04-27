90a7b02d54b2024f30edf3acce2a9b8549b6895a
Mark Rickert <mjar81@gmail.com>
2015-04-27 18:08:10 -0400

    find isn't available in a UICollectionViewCell

--- templates/collection_view_screen/app/views/name_cell.rb
+++ templates/collection_view_screen/app/views/name_cell.rb
@@ -2,9 +2,9 @@ class <%= @name_camel_case %>Cell < UICollectionViewCell
   attr_reader :reused
 
   def on_load
-    find(self).apply_style :<%= @name %>_cell
+    rmq(self).apply_style :<%= @name %>_cell
 
-    q = find(self.contentView)
+    q = rmq(self.contentView)
     # Add your subviews, init stuff here
     # @foo = q.append!(UILabel, :foo)
   end
