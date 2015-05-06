902e8930af22f9d2d70cf1f3b1e4a7479d80abd6
Jamon Holmgren <jamon@clearsightstudio.com>
2015-05-05 21:19:28 -0700

    Add in super call

--- lib/project/pro_motion/table.rb
+++ lib/project/pro_motion/table.rb
@@ -1,8 +1,8 @@
 module ProMotion
   class TableScreen < TableViewController
-    # Don't call super -- would result in two on_load calls to the cell.
     def on_cell_created(cell, data)
       build(cell)
+      super
     end
   end
 end
