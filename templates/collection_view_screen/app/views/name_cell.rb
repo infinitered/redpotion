4b78b1400f4a1c9ceb5dc905052627d6dcc931ec
Mark Rickert <mjar81@gmail.com>
2015-06-11 10:05:33 -0400

    Remove unneeded things from the UICollectionViewCell template.
    They're now included in the UICollectionViewCell extension.

--- templates/collection_view_screen/app/views/name_cell.rb
+++ templates/collection_view_screen/app/views/name_cell.rb
@@ -1,6 +1,4 @@
 class <%= @name_camel_case %>Cell < UICollectionViewCell
-  attr_reader :reused
-
   def on_load
     find(self).apply_style :<%= @name %>_cell
 
@@ -8,10 +6,4 @@ class <%= @name_camel_case %>Cell < UICollectionViewCell
     # Add your subviews, init stuff here
     # @foo = q.append!(UILabel, :foo)
   end
-
-  def prepareForReuse
-    super
-    @reused = true
-  end
-
 end
